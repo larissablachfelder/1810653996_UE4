2ed94f4519d74b1a8afc385ef6e251f0ffa1ac2a
Larissa Blachfelder <1810653996@stud.fh-kufstein.ac.at>
2018-12-11 14:12:17 +0100

    aufgabe 3

---
+++ src/at/ac/fh_kufstein/uebung_04/Aufruf.java
@@ -0,0 +1,4 @@
+package at.ac.fh_kufstein.uebung_04;
+
+public class Aufruf {
+}
---
+++ src/at/ac/fh_kufstein/uebung_04/Book.java
@@ -0,0 +1,57 @@
+package at.ac.fh_kufstein.uebung_04;
+
+import java.util.Date;
+
+public class Book
+{
+    //variablen  festlegen --> referenzvariablen
+   private int pages;
+   private Date released;
+   private  String title;
+   private String isbn;
+
+    //Konstruktor  --> Werte werden auf die Referenzvariablen gespeichert
+    public  Book(int pages, Date released, String title,  String isbn)
+    {
+        this.pages = pages;
+        this.released  = released;
+        this.title = title;
+        this.isbn = isbn;
+    }
+
+    //getter und setter  für alle 4 variablen
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public Date getReleased() {
+        return released;
+    }
+
+    public void setReleased(Date released) {
+        this.released = released;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+
+
+}
