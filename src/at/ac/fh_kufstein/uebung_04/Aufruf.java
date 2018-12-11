1b6a0d02847f9de88329a6935dddbccc31b3af98
Larissa Blachfelder <1810653996@stud.fh-kufstein.ac.at>
2018-12-11 15:33:21 +0100

    aufgabe 5

--- src/at/ac/fh_kufstein/uebung_04/Aufruf.java
+++ src/at/ac/fh_kufstein/uebung_04/Aufruf.java
@@ -10,6 +10,7 @@ public class Aufruf
     public static void main(String[] args)
     {
         //aufgabe 3
+        System.out.println("\nAufgabe 3:");
 
         //118 für das Jahr 2018 da erst ab 1900 die Jahre gezählt  werden; monat -1
         Date datum = new Date(118, 9, 2, 14, 9, 54);
@@ -22,22 +23,49 @@ public class Aufruf
 
 
         //aufgabe 4
+        System.out.println("\nAufgabe 4:");
+
 
         //ich erstelle eine Liste namens liebling
         List<Book> liebling = new ArrayList<>();
 
         //ich erstelle 5 neue variablen und füge  sie der Liste hinzu
-        liebling.add(new Book(200, datum, "Before I go to sleep", "345894-458485-4324"));
-        liebling.add(new Book(160, datum, "Drowning Pool", "43556-47757395-24254"));
+        liebling.add(new Book(200, new Date(), "Before I go to sleep", "345894-458485-4324")); //das heutige Datum
+        liebling.add(new Book(160, datum, "Drowning Pool", "43556-47757395-24254")); //das datum das auf diese variable definiert wurde
         liebling.add(new Book(30, datum, "Nur ein kleiner Gefallen", "385063-355523-345562"));
         liebling.add(new Book(597, datum, "Stirb mein Prinz", "358673-124423512-348743848"));
         liebling.add(new Book(234, datum, "Isola", "6723542-523356236-23643265"));
 
-        System.out.println("Aufgabe 4:");
+
 
         //Instanzenname und zählervariable und listenname
         for(Book i : liebling){
-            System.out.println(i.getTitle()+ " hat "+ i.getPages()+ " Seiten und folgende ISBN: "+i.getIsbn());
+            System.out.println(i.getTitle()+ " hat "+ i.getPages()+ " Seiten und folgende ISBN: "+i.getIsbn() );
         }
+
+
+        //Aufgabe 5 Paperbook
+        System.out.println("\nAufgabe 5:");
+
+
+        List<Paperbook> papierbuch = new ArrayList<>();
+
+
+        papierbuch.add(new Paperbook(134, new Date(), "Kammer des Schreckens", "Harry Potter", "Jänner"));
+        papierbuch.add(new Paperbook(233, new Date(), "Der Hobbit", "Der Hobbit", "Februar"));
+        papierbuch.add(new Paperbook(124, new Date(), "Mockingjay Teil 1", "Die Tribute von Panem", "Oktober"));
+        papierbuch.add(new Paperbook(245, new Date(), "Stein der Weisen", "Harry Potter", "Dezember"));
+        papierbuch.add(new Paperbook(546, new Date(), "Hunger Games", "Die Tribute von Panem", "Juni"));
+
+
+        //Instanzenname und zählervariable und listenname
+        for (Paperbook j : papierbuch){
+            System.out.println(j.getTitle()+ " hat "+ j.getPages()+" Seiten und folgende ISBN: "+ j.getIsbn()+ " zusätzlich ist es im Monat "+j.getMonth()+" erschienen");
+        }
+
+
+
+
+
     }
 }
---
+++ src/at/ac/fh_kufstein/uebung_04/Paperbook.java
@@ -0,0 +1,37 @@
+package at.ac.fh_kufstein.uebung_04;
+
+import java.util.Date;
+
+public class Paperbook extends Book
+{
+    private String series;
+    private String month;
+
+
+    public Paperbook(int pages, Date released, String title, String series, String month)
+    {   //greife hier auf die variablen der vererbten klasse zu
+        super(pages, released, title, "0000-000000-0000"); //jedes buch hat dieselbe isbn deshalb hier definiert
+
+        //greife auf neue Referenzvariablen zu
+        this.series=series;
+        this.month=month;
+
+    }
+
+    //getter und setter der neuen Variablen
+    public String getSeries() {
+        return series;
+    }
+
+    public void setSeries(String series) {
+        this.series = series;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+}
